9b1c1ebc033223a9cb5fae4490a0721cde04930e
marcin.milczek <marcin.milczek@onwelo.com>
2024-09-25 12:55:54 +0200

    Create WireMock in OrderServiceApplicationTests, configuration

--- order-service/src/test/java/com/marcin/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/marcin/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.marcin.microservices.order;
 
+import com.marcin.microservices.order.client.InventoryClient;
+import com.marcin.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
     @ServiceConnection
@@ -39,6 +43,8 @@ class OrderServiceApplicationTests {
                      "quantity": 1
                 }
                 """;
+
+        InventoryClientStub.stubInventoryCall("iPhone_15", 1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
