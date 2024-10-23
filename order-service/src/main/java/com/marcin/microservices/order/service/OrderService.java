29fc836aad9240c38cebde8bd5e83eb33f55a393
marcin.milczek <marcin.milczek@onwelo.com>
2024-10-23 16:02:47 +0200

    Add sending Kafka messeges to OrderService

--- order-service/src/main/java/com/marcin/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/marcin/microservices/order/service/OrderService.java
@@ -2,13 +2,17 @@ package com.marcin.microservices.order.service;
 
 import com.marcin.microservices.order.client.InventoryClient;
 import com.marcin.microservices.order.dto.OrderRequest;
+import com.marcin.microservices.order.event.OrderPlacedEvent;
 import com.marcin.microservices.order.model.Order;
 import com.marcin.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -17,6 +21,8 @@ public class OrderService {
 
     private final OrderMapper orderMapper;
 
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
     public void placeOrder(OrderRequest orderRequest) {
 
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -24,6 +30,11 @@ public class OrderService {
         if (isProductInStock) {
             Order order = orderMapper.toOrder(orderRequest);
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
         }
