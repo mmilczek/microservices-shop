f45392814a0cd392ee701f1912cfafd81b65d5d1
marcin.milczek <marcin.milczek@onwelo.com>
2024-10-25 07:32:16 +0200

    Configuration SecurityConfig class

--- api-gateway/src/main/java/com/programming/marcin/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programming/marcin/gateway/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.programming.marcin.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 public class SecurityConfig {
@@ -14,12 +21,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeRequests(authorize -> authorize
+        return httpSecurity
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
                         .requestMatchers(freeResourceUrls)
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                        .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                         .build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Angular
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // Jeśli używasz sesji lub ciastek
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
