b145d7108cd451dea8eef6e01b9b585331875ccb
marcin.milczek <marcin.milczek@onwelo.com>
2024-10-28 11:11:24 +0100

    Add Confluent and Avro to Order microservice

--- order-service/src/main/java/com/marcin/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/marcin/microservices/order/service/OrderService.java
@@ -31,7 +31,11 @@ public class OrderService {
             Order order = orderMapper.toOrder(orderRequest);
             orderRepository.save(order);
 
-            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
             log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
             kafkaTemplate.send("order-placed", orderPlacedEvent);
             log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
